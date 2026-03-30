5fa8abab18845be98914b4429245ea125e0f7486
Ataiderj <55640402+Ataiderj@users.noreply.github.com>
2026-03-30 18:02:29 -0300

    Criação de uma Landing page Transporte particular

---
+++ script.js
@@ -0,0 +1,6 @@
+// Exemplo inicial
+console.log("Site carregado com sucesso!");
+
+// Futuro: pode colocar aqui
+// animações, botão flutuante inteligente,
+// mensagens automáticas no WhatsApp, etc.
